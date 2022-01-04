19a507e824c8bb14510b3d750d08c7b57ccf9130
sweetsummergone <limfix.13@gmail.com>
2022-01-04 11:11:42 +0200

    feat: add images by javascript

--- scripts/index.js
+++ scripts/index.js
@@ -9,6 +9,48 @@ let userActivity = document.querySelector(".info__whois");
 let modalName = document.querySelector(".modal__input_type_name");
 let modalActivity = document.querySelector(".modal__input_type_whois");
 
+const initialCards = [
+  {
+    name: "Yosemite Valley",
+    link: "https://code.s3.yandex.net/web-code/yosemite.jpg"
+  },
+  {
+    name: "Lake Louise",
+    link: "https://code.s3.yandex.net/web-code/lake-louise.jpg"
+  },
+  {
+    name: "Bald Mountains",
+    link: "https://code.s3.yandex.net/web-code/bald-mountains.jpg"
+  },
+  {
+    name: "Latemar",
+    link: "https://code.s3.yandex.net/web-code/latemar.jpg"
+  },
+  {
+    name: "Vanoise National Park",
+    link: "https://code.s3.yandex.net/web-code/vanoise.jpg"
+  },
+  {
+    name: "Lago di Braies",
+    link: "https://code.s3.yandex.net/web-code/lago.jpg"
+  }
+];
+
+const facebook = document.querySelector(".facebook");
+
+// add content
+initialCards.forEach(card => {
+  const cardTemplate = document.querySelector("#facebook__card").content;
+  
+  // clone the content of the template tag 
+  const cardElement = cardTemplate.querySelector('.facebook__card').cloneNode(true);
+  cardElement.querySelector(".facebook__image").src = card.link;
+  cardElement.querySelector(".facebook__name").textContent = card.name;
+
+  // make it appear on the page
+  facebook.append(cardElement); 
+});
+
 function toggleLike(event) {
   event.currentTarget.classList.toggle("facebook__like_liked");
 }
