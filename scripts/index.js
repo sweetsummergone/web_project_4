668523515086f3903e912021ba286ca4881954c2
sweetsummergone <limfix.13@gmail.com>
2022-01-07 23:49:50 +0200

    fix: javascript fix

--- scripts/index.js
+++ scripts/index.js
@@ -1,20 +1,25 @@
-let buttonEdit = document.querySelector(".info__button-edit");
-let buttonAdd = document.querySelector(".profile__button-add");
-let buttonClose = Array.from(document.querySelectorAll(".modal__button-close"));
-let buttonSave = document.querySelector(".modal__button-save");
-let buttonSavePhoto = document.querySelector(".modal__button-save-photo");
-let modal = Array.from(document.querySelectorAll(".modal"));
-let modalEdit = document.querySelector(".modal_edit");
-let modalAdd = document.querySelector(".modal_add");
-let modalForm = Array.from(document.querySelectorAll(".modal__content"));
-let userName = document.querySelector(".info__name");
-let userActivity = document.querySelector(".info__whois");
-let modalName = document.querySelector(".modal__input_type_name");
-let modalActivity = document.querySelector(".modal__input_type_whois");
-let modalAddTitle = document.querySelector(".modal__input_type_title");
-let modalAddUrl = document.querySelector(".modal__input_type_url");
-
+// 
+const buttonEdit = document.querySelector(".info__button-edit");
+const buttonAdd = document.querySelector(".profile__button-add");
+const buttonSave = document.querySelector(".modal__button-save");
+const buttonSavePhoto = document.querySelector(".modal__button-save-photo");
+// 
 const facebook = document.querySelector(".facebook");
+// 
+const modalEdit = document.querySelector(".modal_edit");
+const modalAdd = document.querySelector(".modal_add");
+const modalName = document.querySelector(".modal__input_type_name");
+const modalActivity = document.querySelector(".modal__input_type_whois");
+const modalAddTitle = document.querySelector(".modal__input_type_title");
+const modalAddUrl = document.querySelector(".modal__input_type_url");
+// 
+const userName = document.querySelector(".info__name");
+const userActivity = document.querySelector(".info__whois");
+// 
+const modalList = Array.from(document.querySelectorAll(".modal"));
+const modalFormList = Array.from(document.querySelectorAll(".modal__content"));
+const buttonCloseList = Array.from(document.querySelectorAll(".modal__button-close"));
+
 const initialCards = [
   {
     name: "Yosemite Valley",
@@ -42,96 +47,89 @@ const initialCards = [
   }
 ];
 
-
 // add content
 initialCards.forEach(card => {
-  addCard(card.link,card.name,"append");
+  renderCard(addCard({url: card.link, name: card.name}));
 });
 
-let buttonDelete = Array.from(document.querySelectorAll(".facebook__delete"));
-let images = Array.from(document.querySelectorAll(".facebook__image"));
+const images = Array.from(document.querySelectorAll(".facebook__image"));
 
 function deleteCard(evt){
   evt.currentTarget.parentNode.remove();
 }
 
 function openImage(evt) {
-  // const modalPopup = document.querySelector("#modal_popup").content;
-
-  // const popupElement = modalPopup.querySelector('.modal_popup').cloneNode(true);
-
-  let modalPopup = document.querySelector(".modal_popup");
-  let modalImage = document.querySelector(".popup__image");
-  let modalTitle = document.querySelector(".popup__title");
-  modalImage.src = evt.currentTarget.src
+  const modalPopup = document.querySelector(".modal_popup");
+  const modalImage = document.querySelector(".popup__image");
+  const modalTitle = document.querySelector(".popup__title");
+  modalImage.src = evt.currentTarget.src;
+  modalImage.alt = evt.currentTarget.alt;
   modalTitle.textContent = evt.currentTarget.alt;
-  modalPopup.classList.toggle("modal_hidden");
+  openModal(modalPopup);
+}
+
+function openModal(modalWindow) {
+  modalWindow.classList.remove("modal_hidden");
 }
 
 function openEdit() {
-  modalEdit.classList.remove("modal_hidden");
+  openModal(modalEdit);
   modalName.value = userName.textContent;
   modalActivity.value = userActivity.textContent;
-  renderInput();
+  toggleButtonState();
 }
 
 function openAdd() {
-  modalAdd.classList.remove("modal_hidden");
+  openModal(modalAdd);
   renderPhotoInput();
 }
 
-function closeModal() {
-  modal.forEach(item => {
-    item.classList.add("modal_hidden");
-  })
+function closeModal(evt) {
+  evt.currentTarget.parentNode.parentNode.classList.add("modal_hidden");
   renderInput();
 }
 
-function addCard(url,name,position) {
+function addCard(data) {
   const cardTemplate = document.querySelector("#facebook__card").content;
   
   // clone the content of the template tag 
   const cardElement = cardTemplate.querySelector('.facebook__card').cloneNode(true);
-  cardElement.querySelector(".facebook__image").src = url;
-  cardElement.querySelector(".facebook__image").alt = name;
-  cardElement.querySelector(".facebook__name").textContent = name;
-  
+  cardElement.querySelector(".facebook__image").src = data.url;
+  cardElement.querySelector(".facebook__image").alt = data.name;
+  cardElement.querySelector(".facebook__name").textContent = data.name;
+  cardElement.querySelector(".facebook__delete").addEventListener("click",deleteCard);
   cardElement.querySelector(".facebook__like").addEventListener("click", function (evt) {
     evt.target.classList.toggle("facebook__like_liked");
   });
-  // make it appear on the page
-  switch(position) {
-    case "append":
-      facebook.append(cardElement);    
-      break;
-    case "prepend":
-      facebook.prepend(cardElement); 
-      break;
-  }
+
+  return cardElement;
+}
+
+function renderCard(card) {
+  facebook.prepend(card); 
 }
 
 function saveChanges(evt) {
   evt.preventDefault();
+
   if(evt.currentTarget.classList.contains("modal__content-edit")) {
     let newName = document.querySelector(".modal__input_type_name");
     let newActivity = document.querySelector(".modal__input_type_whois");
   
-    if (newName.value !== "" && newActivity.value !== "") {
-      userName.innerText = newName.value;
-      userActivity.innerText = newActivity.value;
-    }
+    userName.innerText = newName.value;
+    userActivity.innerText = newActivity.value;
   }
+
   if(evt.currentTarget.classList.contains("modal__content-add")) {
     let url = document.querySelector(".modal__input_type_url").value;
     let title = document.querySelector(".modal__input_type_title").value;
-    if (url !== "" && title !== "") {
-      addCard(url,title,"prepend")
-    }
+    renderCard(addCard({url: url, name: title}));
   }
+
   closeModal();
 }
 
-function renderInput() {
+function toggleButtonState() {
   if (modalName.value === "" || modalActivity.value === "") {
     buttonSave.disabled = true;
     buttonSave.classList.add("modal__button-save_disabled");
@@ -158,24 +156,20 @@ function saveInput() {
 
 buttonEdit.addEventListener("click", openEdit);
 buttonAdd.addEventListener("click", openAdd);
-modalForm.forEach(modal => {
+modalFormList.forEach(modal => {
   modal.addEventListener("submit", saveChanges)
 });
-buttonClose.forEach(button => {
+buttonCloseList.forEach(button => {
   button.addEventListener("click", closeModal);
 });
 buttonSavePhoto.addEventListener("click", addCard)
-buttonDelete.forEach(button => {
-  button.addEventListener("click",deleteCard);
-})
-
 
 images.forEach(img => {
   img.addEventListener("click",openImage)
 })
 
-modalName.addEventListener("input", renderInput);
-modalActivity.addEventListener("input", renderInput);
+modalName.addEventListener("input", toggleButtonState);
+modalActivity.addEventListener("input", toggleButtonState);
 
 modalAddTitle.addEventListener("input", renderPhotoInput);
 modalAddUrl.addEventListener("input", renderPhotoInput);
